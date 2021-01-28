d4c6fe5b078f746cc5a24a95e1d74019a1494178
João_Pedro <joaopedrocz12345@gmail.com>
2021-01-27 23:30:17 -0300

    update Aula 1

---
+++ aluraquiz/pages/index.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import db from '../db.json';
+
+// components
+import Widget from './src/components/Widget'
+import Footer from './src/components/Footer'
+import QuizBackground from './src/components/QuizBackground'
+import QuizContainer from './src/components/QuizContainer'
+import GitHubCorner from './src/components/GitHubCorner'
+
+export default function Home() {
+  return(
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer/>
+      </QuizContainer>
+      <GitHubCorner projectUrl='https://github.com/omariosouto'/>
+    </QuizBackground>
+  );
+}
